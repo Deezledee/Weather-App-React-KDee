dcfba942f05a528d19720bb0e02224ea8ae5504f
Diana Kalstein <96650952+Deezledee@users.noreply.github.com>
2022-02-22 11:55:00 +0100

    Update todayFormatDate.js

--- src/todayFormatDate.js
+++ src/todayFormatDate.js
@@ -1,10 +1,12 @@
-function todayFormatDate(timestamp) {
-  let date = new Date(timestamp);
-  let currentHour = date.getHours();
+import React from "react";
+
+export default function todayFormatDate(props) {
+  let date = new Date(props);
+  let currentHour = props.date.getHours();
   if (currentHour < 10) {
     currentHour = `0${currentHour}`;
   }
-  let currentMinutes = date.getMinutes();
+  let currentMinutes = props.date.getMinutes();
   if (currentMinutes < 10) {
     currentMinutes = `0${currentMinutes}`;
   }
@@ -31,9 +33,9 @@ function todayFormatDate(timestamp) {
     "November",
     "December"
   ];
-  let currentDay = days[date.getDay()];
-  let currentMonth = months[date.getMonth()];
-  let currentDate = date.getDate();
+  let currentDay = days[props.date.getDay()];
+  let currentMonth = months[props.date.getMonth()];
+  let currentDate = props.date.getDate();
   let formattedDate = `${currentDay}, ${currentMonth} ${currentDate}, ${currentHour}:${currentMinutes}`;
   return formattedDate;
 }
@@ -41,10 +43,10 @@ let now = new Date();
 let h3 = document.querySelector(".date");
 h3.innerHTML = `${todayFormatDate(now)}`;
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
+function formatDay(props) {
+  let date = new Date(props * 1000);
+  let day = props.date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  return days[day];
+  return props.days[day];
 }
