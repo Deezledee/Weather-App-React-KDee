bbce976edb4b8ca301d78d92bf199f7ea8e4c8fa
Diana Kalstein <96650952+Deezledee@users.noreply.github.com>
2022-02-22 15:32:38 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,15 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import Weather from "./Weather";
 
-import App from "./App";
 import "./App.css";
 
-function App () {
-  
 function App() {
   return (
     <div className="App">
-    <Weather />
-     </div>
+      <Weather />
+    </div>
   );
 }
-      
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
