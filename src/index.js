72a22c36b32f2fa9089e029ef989a9d9060824e7
Diana Kalstein <96650952+Deezledee@users.noreply.github.com>
2022-02-22 12:10:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import "./App.css";
+
+function App () {
+  
+  return <div className="App" />;
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
