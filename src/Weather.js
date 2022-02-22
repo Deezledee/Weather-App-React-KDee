64aa27020c1619d5acd5f49da8195f7fce214879
Diana Kalstein <96650952+Deezledee@users.noreply.github.com>
2022-02-22 15:33:58 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,18 @@
 import React from "react";
 import "./Weather.css";
 
-export default function Weather () {
-  
+export default function Weather() {
   let weatherData = {
     city: "Tokyo",
     date: "Thursday 30/12/2021 22:30",
     description: "Slight Rain",
     windspeed: 10,
     humidity: 80,
-    temperature: 19
-   
+    temperature: 19,
   };
 
-  return (<div className="Weather">
+  return (
+    <div className="Weather">
       <h1>🌡️ Weather Forecast 🌡️</h1>
 
       <br />
@@ -22,12 +21,13 @@ export default function Weather () {
           type="text"
           className="form-control"
           placeholder="Enter a city name"
-          autocomplete="on"
+          autoComplete="on"
         />
-        <input type="submit" value="Search 🔍" className="form-control" /> <br />
+        <input type="submit" value="Search 🔍" className="form-control" />{" "}
+        <br />
         <button className="btn btn-success">
-          Current location <span>📍<span/> 
-          </button>
+          Current location <span>📍</span>
+        </button>
       </form>
       <br />
 
@@ -36,15 +36,13 @@ export default function Weather () {
       <p>
         <span className="today-temperature">19</span>
         <span className="units">
-          <a href="#" >°C</a> |<a href="#"
-            >°F</a
-          ></span
-        >
+          <a href="#">°C</a> |<a href="#">°F</a>
+        </span>
         <br />
       </p>
 
       <div className="todayDescription">
-        <span >{weatherData.description}</span>
+        <span>{weatherData.description}</span>
         <img
           src="http://openweathermap.org/img/wn/10d@2x.png"
           alt="clear sky"
@@ -55,7 +53,7 @@ export default function Weather () {
       <br />
       <div className="row paremeters">
         <div className="col humidity">
-          💧 Humidity: <span >{weatherData.humidity}</span>%
+          💧 Humidity: <span>{weatherData.humidity}</span>%
         </div>
         <div className="col windspeed">
           💨 Windspeed:
@@ -66,24 +64,28 @@ export default function Weather () {
       <h3 className="date">{weatherData.date}</h3>
       <br />
 
-
       <div className="weather-forecast">
-          <div className="row">
-            <div className="col-2">
-              <div className="weather-forecast-date">Thu</div>
-              <img
-                src="http://openweathermap.org/img/wn/50d@2x.png"
-                alt=""
-                width="40"
-              />
-              <div className="weather-forecast-temperatures">
-                <span className="weather-forecast-temperature-max"> {weatherData.temperature}° </span>
-                <span className="weather-forecast-temperature-min"> {weatherData.temperature}° </span>
-              </div>
+        <div className="row">
+          <div className="col-2">
+            <div className="weather-forecast-date">Thu</div>
+            <img
+              src="http://openweathermap.org/img/wn/50d@2x.png"
+              alt=""
+              width="40"
+            />
+            <div className="weather-forecast-temperatures">
+              <span className="weather-forecast-temperature-max">
+                {" "}
+                {weatherData.temperature}°{" "}
+              </span>
+              <span className="weather-forecast-temperature-min">
+                {" "}
+                {weatherData.temperature}°{" "}
+              </span>
             </div>
           </div>
         </div>
       </div>
-
-  )
+    </div>
+  );
 }
